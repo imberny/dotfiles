b7ec1561ca42ee211f42369cad330d318491599e
imberny <bernard.p.cloutier@gmail.com>
2024-09-07 20:32:52 -0400

    Remove vindent, replace with various-textobjs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,25 +18,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
--- My vindent.vim settings
-vim.g.vindent_motion_OO_prev = "[l"
-vim.g.vindent_motion_OO_next = "]l"
-vim.g.vindent_motion_less_prev = "[-"
-vim.g.vindent_motion_less_next = "]-"
-vim.g.vindent_motion_more_prev = "[="
-vim.g.vindent_motion_more_next = "]="
-vim.g.vindent_motion_XX_ss = "[p"
-vim.g.vindent_motion_XX_se = "]p"
-vim.g.vindent_motion_OX_ss = "[P"
-vim.g.vindent_motion_OX_se = "]P"
-vim.g.vindent_object_OO_ii = "iI"
-vim.g.vindent_object_XX_ii = "ii"
-vim.g.vindent_object_XX_ai = "ai"
-vim.g.vindent_object_XX_aI = "aI"
-vim.g.vindent_jumps = 1
-vim.g.vindent_count = 0
-vim.g.vindent_begin = 1
-
 require "lazy_setup"
 require "polish"
 -- TODO: figure out how to load config folder instead of individual files
--- nvim/.config/nvim/lua/plugins/vindent.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "jessekelighine/vindent.vim",
-}
