dd62a4869584577db1d68807e7653d73d7c070bc
imberny <bernard.p.cloutier@gmail.com>
2024-10-03 00:28:07 -0400

    Add source for godot config

--- nvim/.config/nvim/lua/config/gdscript.lua
+++ nvim/.config/nvim/lua/config/gdscript.lua
@@ -1,21 +1,22 @@
-require("lspconfig")["gdscript"].setup({
-	name = "godot",
-	cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-})
+-- https://www.reddit.com/r/neovim/comments/1c2bhcs/godotgdscript_in_neovim_with_lsp_and_debugging_in/
+require("lspconfig")["gdscript"].setup {
+  name = "godot",
+  cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+}
 
-local dap = require("dap")
+local dap = require "dap"
 dap.adapters.godot = {
-	type = "server",
-	host = "127.0.0.1",
-	port = 6006,
+  type = "server",
+  host = "127.0.0.1",
+  port = 6006,
 }
 
 dap.configurations.gdscript = {
-	{
-		type = "godot",
-		request = "launch",
-		name = "Launch scene",
-		project = "${workspaceFolder}",
-		launch_scene = true,
-	},
+  {
+    type = "godot",
+    request = "launch",
+    name = "Launch scene",
+    project = "${workspaceFolder}",
+    launch_scene = true,
+  },
 }
